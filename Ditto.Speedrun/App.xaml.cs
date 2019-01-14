4523bce15e45c31550a3ca9247a79e9a8841d4fe
Deecellar <19101das@gmail.com>
2019-01-14 15:49:20 -0500

    Add files via upload

---
+++ Ditto.Speedrun/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Reflection;
+
+namespace Ditto.Speedrun
+{
+    /// <summary>
+    /// Not using MVVM SMH
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Using the https://gist.github.com/aksakalli/9191056 SimpleHTTPServer
+        /// </summary>
+        private static SimpleHTTPServer server = new SimpleHTTPServer(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DittoPage");
+
+        public App()
+        {
+            this.Exit += App_Exit;
+        }
+        /// <summary>
+        /// The property to access the server from outside the App class
+        /// </summary>
+        internal static SimpleHTTPServer Server { get => server;}
+        /// <summary>
+        /// Close the conection with the server to not let the port opened after it's closed
+        /// also ensures that the app closes correctly and it's not open after  it
+        /// </summary>
+
+        private void App_Exit(object sender, ExitEventArgs e)
+        {
+            Server.Stop();
+            App.Current.Shutdown();
+        }
+
+
+    }
+}
---
+++ Ditto.Speedrun/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace Ditto.Speedrun
+{
+    /// <summary>
+    /// Not using MVVM SMH
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// We start the main window with a populated combobox, directed to The localhost port where the game is hosted
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Populate();
+            Content.Navigate("http://localhost:" + App.Server.Port.ToString());
+            Closing += MainWindow_Closing;
+            Title = "Ditto with saves running on " + "http://localhost:" + App.Server.Port.ToString();
+        }
+        /// <summary>
+        /// Avoid Ditto.Speedrun to live after it's done
+        /// </summary>
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+        /// <summary>
+        /// Method to change between changes, since Local Shared Objects are saved on 
+        /// C:\Users\<Username>\AppData\Roaming\Macromedia\Flash Player\#SharedObjects
+        /// we look at the file we need to save, in this case ditto.swf and change them
+        /// with a local save made for it
+        /// </summary>
+        private void ChangeSave()
+        {
+            string appdata = Environment.ExpandEnvironmentVariables("%AppData%");
+            string path =  appdata + @"\Macromedia\Flash Player\#SharedObjects\";
+
+            string list = Directory.GetDirectories(path, "#localhost", SearchOption.AllDirectories).First() + "\\ditto.swf";
+            string pathSave = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DittoSaves\" + Level.SelectedItem + "\\" + Room.SelectedItem + ".sol";
+
+            File.Copy(pathSave, list + "\\so_ditto1.sol", true);
+        }
+        /// <summary>
+        /// Change the save, Reload the page and fix this weird bug not finding the reload page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cargar_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeSave();
+            Content.Navigate("http://localhost:" +App.Server.Port.ToString());
+        }
+        /// <summary>
+        /// Populate the Level ComboBox from the directories of DittoSaves
+        /// </summary>
+            private void Populate()
+        {
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DittoSaves\";
+            string[] paths = Directory.GetDirectories(path).Select(s => s.Remove(0,path.Length)).ToArray();
+            Level.ItemsSource = paths;
+            Level.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// When level changes, populate the Room selection with the contents of the Directory equivalent from DittoSaves
+        /// </summary>
+
+        private void Level_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DittoSaves\" + Level.SelectedItem;
+            string[] rooms = Directory.GetFiles(path, "*.sol*").Select(Path.GetFileNameWithoutExtension).Select(p => p.Substring(0)).ToArray();
+            Array.Sort(rooms, StringComparer.OrdinalIgnoreCase);
+            Room.ItemsSource = rooms;
+            Room.SelectedIndex = 0;
+        }
+        
+    }
+}
